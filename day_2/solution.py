d8845ca0b2dafcb2a073f8f734e8dbc3a9a092f4
Joseph Haaga <haaga.joe@gmail.com>
2023-12-04 15:28:38 -0500

    day2: part 1 done

--- day_2/solution.py
+++ day_2/solution.py
@@ -5,14 +5,17 @@ from typing import Tuple
 
 
 
-def parse_game_record(record: str) -> List[Tuple[int, str]]:
+def parse_game_record(record: str) -> List[Dict[str, int]]:
     """Extract observations from a single game record."""
-    observations_pattern = re.compile(r' ((\d+) ([a-z]+))[,;]?')
-    observations: List[Tuple[int, str]] = []
-    for m in observations_pattern.findall(record):
-        observations += [(int(m[1]), m[2])]
-    return observations
-
+    pat = re.compile(r'((\d+) (\w+))')
+    r = record[record.index(":") + 1:]
+    results = []
+    for obs in r.split(";"):
+        results += [{
+            match[2]: int(match[1])
+            for match in pat.findall(obs)
+        }]
+    return results
 
 def parse_constraint(constraint: str) -> List[Tuple[int, str]]:
     constraint_pattern = re.compile(r'((\d+) ([a-z]+) cubes)')
@@ -34,19 +37,17 @@ def summarize_color_observations(record: List[Tuple[int, str]]) -> Dict[str, int
 
 def game_is_possible(record: str, constraint: str) -> bool:
     """Return whether the game record is possible given the constraint."""
-    obs = summarize_color_observations(
-        parse_game_record(record)
-    )
+    observations = parse_game_record(record)
     cons = summarize_color_observations(
         parse_constraint(constraint)
     )
 
-    for (color, count) in obs.items():
-        if color not in cons:
-            return False
-        if count > cons[color]:
-            return False
-        cons[color] -= count
+    for obs in observations:
+        for (color, count) in obs.items():
+            if color not in cons:
+                return False
+            if count > cons[color]:
+                return False
     return True
 
 
@@ -56,13 +57,11 @@ def main() -> int:
     constraint = "12 red cubes, 13 green cubes, 14 blue cubes"
     number_pattern = re.compile(r'Game (\d+):')
 
-    breakpoint()
     part_1 = 0
     for line in lines:
         game_number = int(number_pattern.match(line).groups(1)[0])
         possible = game_is_possible(line, constraint)
         if possible:
-            print(game_number)
             part_1 += game_number
     print(f"{part_1=}")
 
