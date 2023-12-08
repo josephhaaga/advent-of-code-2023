661948dcb8c4e0d71e0b1757356c606dca13bcc5
Joseph Haaga <haaga.joe@gmail.com>
2023-12-07 19:26:30 -0500

    add new helper function but still not working

--- day_3/solution.py
+++ day_3/solution.py
@@ -1,18 +1,60 @@
+import re
 from typing import List
 from typing import Tuple
 
 
+def border_indexes(row, start, end, width, height) -> List[Tuple[int, int]]:
+    res = []
+
+    min_row, max_row = max(0, row-1), min(row+1, height-1)
+    min_col, max_col = max(0, start-1), min(end+1, width-1)
+    prohibited = [(row, c) for c in range(start, end+1)]
+
+    for r in range(min_row, max_row + 1):
+        for c in range(min_col, max_col + 1):
+            if (r, c) not in prohibited:
+                res += [(r, c)]
+
+    return res
+
+
+def is_part_number(lines: List[str], row: int, col: Tuple[int, int]) -> bool:
+    """Determine if the number at lines[row][col[0]:col[1]] is a Part Number."""
+    l, r = col
+    cols = range(
+        max(0, l-1),
+        min(r+2, len(lines[0]))
+    )
+    rows = range(
+        max(0, row-1),
+        min(row+2, len(lines))
+    )
+    for i in rows:
+        for j in cols:
+            currently_on_original_number = (i == row and l < j < r)
+            if currently_on_original_number:
+                continue
+            char = lines[i][j]
+            char_is_a_symbol = (char != "." and not char.isnumeric())
+            if char_is_a_symbol:
+                return True
+    return False
+
 
 def part_1(lines: List[str]) -> int:
-    height = len(lines)
-    width = len(lines[0])
-
-    row = 0
-    while row < height:
-        
-        row += 1
-
-    return 1
+    s = 0
+    pat = re.compile(r'(\d+)')
+    for row, line in enumerate(lines):
+        row_total = 0
+        matches: List[re.Match] = pat.finditer(line)
+        for match in matches:
+            cols = match.span()
+            number = line[cols[0]:cols[1]]
+            part_number = is_part_number(lines, row, cols)
+            if part_number:
+                row_total += int(number)
+        s += row_total
+    return s
 
 
 def main() -> int:
--- tests/test_day_3.py
+++ tests/test_day_3.py
@@ -1,4 +1,5 @@
 from day_3.solution import part_1
+from day_3.solution import border_indexes
 
 import pytest
 
@@ -22,3 +23,18 @@ PART_1_ANSWER = 4361
 def test_part_1():
     lines = PART_1_INPUT.split("\n")[1:]
     assert part_1(lines) == PART_1_ANSWER
+
+
+def test_border_indexes():
+    assert border_indexes(row=1, start=1, end=2, width=10, height=10) == [
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (0, 3),
+        (1, 0),
+        (1, 3),
+        (2, 0),
+        (2, 1),
+        (2, 2),
+        (2, 3)
+    ]
